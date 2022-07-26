38e732f9c75b04a9f3b111d2d6e5be7bb131c45e
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-07-26 16:27:03 +0530

    loadworks init

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 document.addEventListener('readystatechange', () => {
     if (document.readyState === "complete") {
         greet();
+        loadWorks()
     }
 })
 
@@ -10,4 +11,32 @@ function greet(): void {
     if (HOUR >= 5) {
         greetElement.innerHTML = HOUR >= 12 ? 'afternoon' : 'morning'
     }
+}
+
+async function loadWorks() {
+    type work = {
+        name: string,
+        description: string,
+        link: string,
+        thumb: string
+    }
+    const works: [work] = await fetch('/works.json')
+        .then(response => response.json())
+
+    const workContainer = document.querySelector('#_works')
+    const regxUrl = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/
+    works.forEach(work => {
+        var container = document.createElement('div')
+        var title = document.createElement('h3')
+        var description = document.createElement('p')
+        if (work.thumb.match(regxUrl) !== null) container.style.backgroundImage = encodeURI(work.thumb)
+
+        title.innerText = work.name
+        description.innerText = work.description
+        container.appendChild(title)
+        container.appendChild(description)
+
+        workContainer.appendChild(container)
+    })
+
 }
