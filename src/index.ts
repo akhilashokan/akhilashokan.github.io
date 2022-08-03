a03ecd5fcf74721c13a0bc1fc27d6d73d5e71840
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-08-03 13:55:55 +0530

    font added work sans + gsap cdn + loadmore() added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 document.addEventListener('readystatechange', () => {
     if (document.readyState === "complete") {
-        greet();
+        greet()
         loadWorks()
     }
 })
@@ -8,7 +8,7 @@ document.addEventListener('readystatechange', () => {
 function greet(): void {
     const HOUR: number = new Date().getHours()
     const greetElement = document.getElementById('__greeting')
-    if (HOUR >= 5) {
+    if (HOUR >= 5 && HOUR <= 19) {
         greetElement.innerHTML = HOUR >= 12 ? 'afternoon' : 'morning'
     }
 }
@@ -29,14 +29,38 @@ async function loadWorks() {
         var container = document.createElement('div')
         var title = document.createElement('h3')
         var description = document.createElement('p')
-        if (work.thumb.match(regxUrl) !== null) container.style.backgroundImage = encodeURI(work.thumb)
 
+        if (work.thumb.match(/\.(jpg|png|webp)$/) !== null) container.setAttribute('thumb', encodeURI(work.thumb))
+
+        if (work.link !== null) {
+            var _link = document.createElement('a')
+            _link.setAttribute('href', work.link)
+            _link.setAttribute('target', 'blank')
+            container.appendChild(_link)
+        }
         title.innerText = work.name
         description.innerText = work.description
         container.appendChild(title)
         container.appendChild(description)
+        container.setAttribute('name', work.name)
+        container.classList.add('work')
 
         workContainer.appendChild(container)
     })
 
+
+}
+function loadMore(e: HTMLElement) {
+    e.remove()
+    var text = 'I am interested in all kinds of development,so far i have dabbled with android (java), flutter (Windows), react, and python (tkinter/flask) to build apps. I really appreciate learning about new things and how things function behind the scenes.'
+    var para = document.querySelector('.about p')
+    var array = text.trim().split(" ")
+    array.forEach(item => {
+        var t = document.createElement('span')
+        t.classList.add('_spans')
+        t.innerText = item + ' '
+        para.appendChild(t);
+    })
+    gsap.to('._spans', { display: 'inline', stagger: .05 })
+    gsap.to('._spans', { opacity: 1, stagger: .05 })
 }
