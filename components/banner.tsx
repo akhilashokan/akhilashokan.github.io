1585f9a791cd33e47e8f8563a28c47f32db8a228
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-08-17 21:40:23 +0530

    greet function in useeffecct

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,18 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function Banner() {
-    var greeting = greet()
-    function greet(): string {
-        const HOUR: number = new Date().getHours()
-        var greet = 'evening'
-        if (HOUR >= 5 && HOUR <= 19) {
-            greet = HOUR >= 12 ? 'afternoon' : 'morning'
+    const [greet, setGreet] = useState('evening')
+    useEffect(() => {
+        function greetUser(): void {
+            const HOUR: number = new Date().getHours()
+            if (HOUR >= 5 && HOUR <= 19) {
+                HOUR >= 12 ? setGreet('afternoon') : setGreet('morning')
+            }
         }
-        return greet;
-    }
+        greetUser()
+    }, [])
     return (
         <section className="banner">
             <div className="wrapper">
                 <h1 className="intro" attr-craft="web developer">
-                    <span className="__hello">Hello</span> there! good {greeting} <br />
+                    <span className="__hello">Hello</span> there! good {greet} <br />
                     <span className="__name">
                         <span className="__iam">Iam </span>
                         Akhil Ashokan
