5d6442f8b91bbcb8dbc78d9202a413af86b87001
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-08-18 16:07:46 +0530

    -svg width auto +font size changes

--- components/contact/contact.tsx
+++ components/contact/contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
                 <div className={style.method}>
                     <h3>connect.akhilashokan@gmail.com
                         <a href="mailto:connect.akhilashokan@gmail.com">
-                            <svg xmlns="http://www.w3.org/2000/svg" width={'auto'} height={'100%'} fill="none" stroke="#333" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
+                            <svg xmlns="http://www.w3.org/2000/svg" height={'100%'} fill="none" stroke="#333" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24">
                                 <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6m4-3h6v6m-11 5L21 3" />
                             </svg>
                         </a>
