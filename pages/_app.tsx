f134390b4776141805d1e67d528935c73c3dc581
Akhil Ashokan <52566622+akhilashokan@users.noreply.github.com>
2022-08-18 18:35:43 +0530

    title +favicon fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,15 @@
 import "./style.scss"
+import Head from "next/head"
 export default function Portfolio({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <title>Akhil Ashokan</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
