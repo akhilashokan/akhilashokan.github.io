068610dbc10cd88b01305dbff19dab73a1226f2c
akhilashokan <akhilashokan1996@gmail.com>
2025-06-14 15:07:40 +0530

    refactor: font update

--- components/banner.tsx
+++ components/banner.tsx
@@ -14,15 +14,13 @@ export default function Banner() {
     }, [])
     return (
         <section className="h-[90vh] flex items-center">
-            <div className="">
-                <h1 className="font-primary text-2xl md:text-4xl lg:text-6xl/tight relative">
-                    <span className="max-md:text-8xl max-md:block">Hello</span> there! good {greet} <br />
-                    <span className="max-md:block max-md:mt-[20vh] max-md:text-5xl">
-                        <span className="max-md:block max-md:text-base">Iam </span>Akhil Ashokan
-                    </span>
-                    <span className="absolute left-0 bottom-0 transform-[translateY(100%)] text-base md:text-2xl">web developer</span>
-                </h1>
-            </div>
+            <h1 className="font-primary text-2xl/tight md:text-6xl/tight lg:text-7xl/tight relative">
+                <span className="max-md:text-8xl max-md:block">Hello</span> there! good {greet} <br />
+                <span className="max-md:block max-md:mt-[20vh] max-md:text-5xl">
+                    <span className="max-md:block max-md:text-base">Iam </span>Akhil Ashokan
+                </span>
+                <span className="absolute left-0 bottom-0 transform-[translateY(100%)] text-base md:text-2xl">web developer</span>
+            </h1>
         </section>
     );
 }
